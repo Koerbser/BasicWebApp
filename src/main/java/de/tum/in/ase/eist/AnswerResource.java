2a9d312e072401fd576e8ae7d0f77706dfd253ee
Koerbser <76958302+Koerbser@users.noreply.github.com>
2022-07-14 12:57:32 +0200

    Added Comment "Übertreib mal nicht!"

--- src/main/java/de/tum/in/ase/eist/AnswerResource.java
+++ src/main/java/de/tum/in/ase/eist/AnswerResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api", produces = {MediaType.TEXT_PLAIN_VALUE})
 public class AnswerResource {
 
+	//Übertreib mal nicht!
 	private final Logger logger = LoggerFactory.getLogger(AnswerResource.class);
 
 	private final QueryProcessor queryProcessor;
